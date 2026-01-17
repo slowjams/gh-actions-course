7c47a98defb06612e3b4a957daf34e3b08700e87
slowjams <slowjamsgrooveline@gmail.com>
2026-01-17 11:53:15 +1100

    add first js custom action

---
+++ .github/actions/js-dependency-update/index.js
@@ -0,0 +1,17 @@
+/*
+@actions/core is a Node.js library provided by GitHub for writing custom GitHub Actions. It offers APIs to interact with workflow inputs, outputs, environment variables, and logging. With @actions/core, you can:
+
+- Get and set action inputs and outputs
+- Set environment variables
+- Mark actions as failed or succeeded
+- Log messages at different levels (info, warning, error)
+- Group logs for better readability
+*/
+
+const core = require('@actions/core');
+
+async function run() {
+    core.info('I am a custom JS action');
+}
+
+run()
